d089f632290c97cd685ee3fce65995a1a222f63b
Gonzalo Ojeda <gonzalo.ojeda.m@gmail.com>
2026-09-11 13:23:59 -0300

    feat: se agrega migracion V3 de Flyway para tabla usuarios y limpiar paquetes

---
+++ src/main/resources/db/migration/V3__create_usuarios_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    rol VARCHAR(20) NOT NULL,
+    activo BOOLEAN NOT NULL DEFAULT TRUE
+);
